d9ed34489f04c4785d0c17b715481c2963c00bd3
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-11-08 22:23:28 +0900

    feat: make partition function

--- core/src/main/scala/Block.scala
+++ core/src/main/scala/Block.scala
@@ -45,4 +45,25 @@ class Block(val records: LazyList[Record]) extends AnyVal {
       // TODO: Handle exceptions
     } finally writer.close()
   }
+
+  def partition(workers: List[WorkerMetadata]): List[Partition] = {
+    def isInKeyRange(key: Key, range: KeyRange): Boolean =
+      (key >= range._1) && (key <= range._2)
+
+    val groupedRecords = for {
+      worker <- workers
+      keyRange = worker.keyRange.getOrElse(
+        throw new AssertionError("KeyRange must be defined")
+      )
+      filteredRecords = records.filter(record =>
+        isInKeyRange(record.key, keyRange)
+      )
+    } yield new Partition(
+      worker,
+      new Block(filteredRecords)
+    )
+
+    groupedRecords
+  }
+
 }
