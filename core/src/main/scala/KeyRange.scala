778f9fb06c45faf6efcaad42f30a44dc22df31e6
Minjae Gwon <beta.underalpha@gmail.com>
2023-11-13 17:27:11 +0800

    fix: make range include end of entry

--- core/src/main/scala/KeyRange.scala
+++ core/src/main/scala/KeyRange.scala
@@ -1,7 +1,7 @@
 package kr.ac.postech.paranode.core
 
 case class KeyRange(from: Key, to: Key) {
-  def includes(key: Key): Boolean = (from <= key) && (key < to)
+  def includes(key: Key): Boolean = (from <= key) && (key <= to)
 
   def includes(record: Record): Boolean = includes(record.key)
 }
