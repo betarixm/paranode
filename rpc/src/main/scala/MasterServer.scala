23001e72c9ef2554a6f4f82d4a298d6c116eed20
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-11-14 21:40:45 +0900

    check gRPC is working

---
+++ rpc/src/main/scala/MasterServer.scala
@@ -0,0 +1,76 @@
+package kr.ac.postech.paranode.rpc
+
+import java.util.logging.Logger
+
+import io.grpc.{Server, ServerBuilder}
+import kr.ac.postech.paranode.rpc.master.{
+  MasterGrpc,
+  MasterProto,
+  RegisterRequest,
+  RegisterRelpy
+}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object MasterServer {
+  private val logger = Logger.getLogger(classOf[MasterServer].getName)
+
+  def main(args: Array[String]): Unit = {
+    val server = new MasterServer(ExecutionContext.global)
+    server.start()
+    server.blockUntilShutdown()
+  }
+
+  private val port = 50051
+}
+
+class MasterServer(executionContext: ExecutionContext) { self =>
+  private[this] var server: Server = null
+
+  private def start(): Unit = {
+    server = ServerBuilder
+      .forPort(MasterServer.port)
+      .addService(MasterGrpc.bindService(new MasterImpl, executionContext))
+      .build
+      .start
+    MasterServer.logger.info(
+      "Server started, listening on " + MasterServer.port
+    )
+    sys.addShutdownHook {
+      System.err.println(
+        "*** shutting down gRPC server since JVM is shutting down"
+      )
+      self.stop()
+      System.err.println("*** server shut down")
+    }
+  }
+
+  private def stop(): Unit = {
+    if (server != null) {
+      server.shutdown()
+    }
+  }
+
+  private def blockUntilShutdown(): Unit = {
+    if (server != null) {
+      server.awaitTermination()
+    }
+  }
+
+  private class MasterImpl extends MasterGrpc.Master {
+    override def registerWorkerDirectory(request: RegisterRequest) = {
+      System.err.println("*** server side code working")
+      System.err.println(
+        s"*** Received registration request: ipAddress = ${request.ipAddress}"
+      )
+      System.err.println(
+        s"*** Input Directories: ${request.inputDirectory.mkString(", ")}"
+      )
+      System.err.println(s"*** Output Directory: ${request.outputDirectory}")
+
+      val reply = RegisterRelpy(isRegistered = true)
+      Future.successful(reply)
+    }
+  }
+
+}
