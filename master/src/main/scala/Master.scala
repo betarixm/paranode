e71e75915a621d65864ed60778e0b00b48a0df24
Minjae Gwon <beta.underalpha@gmail.com>
2023-11-26 20:08:36 +0900

    feat: sort and partition

--- master/src/main/scala/Master.scala
+++ master/src/main/scala/Master.scala
@@ -1,6 +1,7 @@
 package kr.ac.postech.paranode.master
 
 import kr.ac.postech.paranode.core.Key
+import kr.ac.postech.paranode.core.KeyRange
 import kr.ac.postech.paranode.core.WorkerMetadata
 import kr.ac.postech.paranode.rpc.MasterServer
 import kr.ac.postech.paranode.rpc.WorkerClient
@@ -20,7 +21,7 @@ object Master extends Logging {
     }
 
     val server = new MasterServer(scala.concurrent.ExecutionContext.global)
-    server.startServer()
+    server.start()
 
     while (server.getWorkerDetails.size < numberOfWorker) {
       Thread.sleep(1000)
@@ -33,7 +34,7 @@ object Master extends Logging {
     try {
       val ipAddress = InetAddress.getLocalHost.getHostAddress
 
-      println(ipAddress + ":" + server.getPort)
+      println(ipAddress + ":" + server.port)
       println(workerInfo.map(_.host).mkString(", "))
     } catch {
       case e: Exception => e.printStackTrace()
@@ -52,6 +53,38 @@ object Master extends Logging {
       .map(Key.fromByteString)
 
     logger.debug(s"Sampled keys: $sampledKeys")
+
+    val sortedSampledKeys = sampledKeys.sorted
+
+    logger.debug(s"Sorted Sampled keys: $sortedSampledKeys")
+
+    val keyRanges = sortedSampledKeys
+      .sliding(
+        sortedSampledKeys.size / numberOfWorker,
+        sortedSampledKeys.size / numberOfWorker
+      )
+      .toList
+      .map(keys => (keys.head, keys.last))
+
+    val keyRangesWithWorker = workerInfo.zip(keyRanges.map(KeyRange.tupled))
+
+    logger.debug(s"Key ranges with worker: $keyRangesWithWorker")
+
+    Await.result(
+      Future.sequence(
+        clients.map(_.sort())
+      ),
+      scala.concurrent.duration.Duration.Inf
+    )
+
+    Await.result(
+      Future.sequence(
+        clients.map(_.partition(keyRangesWithWorker))
+      ),
+      scala.concurrent.duration.Duration.Inf
+    )
+
+    server.blockUntilShutdown()
   }
 
 }
