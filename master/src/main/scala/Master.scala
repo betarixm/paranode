9c3367d75fdb9890113d8f7c46120b7f4f72725a
Minjae Gwon <beta.underalpha@gmail.com>
2023-11-28 15:43:16 +0900

    refactor: separate key estimate logic

--- master/src/main/scala/Master.scala
+++ master/src/main/scala/Master.scala
@@ -1,7 +1,6 @@
 package kr.ac.postech.paranode.master
 
 import kr.ac.postech.paranode.core.Key
-import kr.ac.postech.paranode.core.KeyRange
 import kr.ac.postech.paranode.core.WorkerMetadata
 import kr.ac.postech.paranode.rpc.MasterServer
 import kr.ac.postech.paranode.rpc.WorkerClient
@@ -11,28 +10,6 @@ import java.net._
 import scala.concurrent.ExecutionContextExecutor
 
 object Master extends Logging {
-  private def workersWithKeyRange(
-      keys: List[Key],
-      workers: List[WorkerMetadata],
-      min: Key,
-      max: Key
-  ): List[WorkerMetadata] = {
-    val keysWithLowerBound = keys :+ min
-
-    val startKeys = keysWithLowerBound.sorted
-      .grouped(
-        (keysWithLowerBound.size.toDouble / workers.size.ceil).ceil.toInt
-      )
-      .toList
-      .map(_.head)
-
-    val pairs = startKeys.zip(startKeys.tail.map(_.prior) :+ max)
-
-    workers.zip(pairs).map { case (worker, (min, max)) =>
-      worker.copy(keyRange = Some(KeyRange(min, max)))
-    }
-  }
-
   def main(args: Array[String]): Unit = {
     val masterArguments = new MasterArguments(args)
     val masterHost = InetAddress.getLocalHost.getHostAddress
@@ -57,17 +34,18 @@ object Master extends Logging {
       Thread.sleep(1000)
     }
 
-    val workerInfo: List[WorkerMetadata] = server.registeredWorkers
+    val registeredWorkers: List[WorkerMetadata] = server.registeredWorkers
 
-    println(workerInfo.map(_.host).mkString(", "))
+    println(registeredWorkers.map(_.host).mkString(", "))
 
-    val clients = workerInfo.map { worker =>
+    val clients = registeredWorkers.map { worker =>
       WorkerClient(worker.host, worker.port)
     }
 
     implicit val requestExecutionContext: ExecutionContextExecutor =
       scala.concurrent.ExecutionContext.fromExecutor(
-        java.util.concurrent.Executors.newFixedThreadPool(workerInfo.size)
+        java.util.concurrent.Executors
+          .newFixedThreadPool(registeredWorkers.size)
       )
 
     logger.info(s"[Master] Clients: $clients")
@@ -81,8 +59,13 @@ object Master extends Logging {
 
     logger.info(s"[Master] Sampled $sampledKeys")
 
-    val workers =
-      workersWithKeyRange(sampledKeys, workerInfo, Key.min(), Key.max())
+    val workers = sampledKeys
+      .rangesBy(registeredWorkers.size, Key.min(), Key.max())
+      .map(Some(_))
+      .zip(registeredWorkers)
+      .map { case (keyRange, worker) =>
+        worker.copy(keyRange = keyRange)
+      }
 
     logger.info(s"[Master] Key ranges with worker: $workers")
 
