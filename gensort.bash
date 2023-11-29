8a8666ec5fe2118a61ba730ac2850e20cc55d096
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-11-29 21:19:59 +0900

    feat: add script

--- gensort.bash
+++ gensort.bash
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-# Number of data directories (M) and files in each directory (N)
-numberOfDirectories=2
-numberOfFiles=2
-numberOfRecords=4000
+# Check if sufficient arguments are provided
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <numberOfDirectories> <numberOfFiles> <numberOfRecords>"
+    exit 1
+fi
+
+# Assign the arguments to variables
+numberOfDirectories=$1
+numberOfFiles=$2
+numberOfRecords=$3
 
 # Base directory
 baseDir="/home/green/434project"
 
-# Step 2: Remove files in each data directory and in the output directory
+# Step 2: Check and create directories if needed, then remove files in each data directory and in the output directory
 for (( dir=0; dir<numberOfDirectories; dir++ )); do
-    rm -rf "${baseDir}/data${dir}"/*
+    dataDir="${baseDir}/data${dir}"
+    # Check if directory exists, if not, create it
+    mkdir -p "$dataDir"
+    # Remove files in the directory
+    rm -rf "${dataDir}"/*
 done
 
+# Checking and creating the output directory
+outputDir="${baseDir}/output"
+mkdir -p "$outputDir"
 # Removing files in the output directory
-rm -rf "${baseDir}/output"/*
+rm -rf "${outputDir}"/*
 
 # Step 3: Run the command for each file in each data directory
 for (( dir=0; dir<numberOfDirectories; dir++ )); do
